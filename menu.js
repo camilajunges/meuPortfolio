7f7b3b87890249cca12f1a7eb6b7287d86ab2b84
camilajunges <camilasjunges@gmail.com>
2023-07-09 15:39:42 -0300

    resp

---
+++ menu.js
@@ -0,0 +1,20 @@
+var menuItem = document.querySelectorAll('.item-menu');
+
+function selectLink(){
+    menuItem.forEach((item)=>
+    item.classList.remove('ativo'))
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectLink);
+)
+
+// vai expandir o menu
+
+var btnExp = document.querySelector('#btn-exp')
+var menuTop = document.querySelector('.navbar-top')
+
+btnExp.addEventListener('click', function(){
+    menuTop.classList.toggle('expandir');
+})
